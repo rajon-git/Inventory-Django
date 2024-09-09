bd27a4edd8ead1e6c1e49273fb8345ae08d6aff5
rajon-git <rajon.zhsust15@gmail.com>
2024-09-09 17:25:09 +0600

    logout user and update profile some functionality added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -10,4 +12,7 @@ urlpatterns = [
     path('profile/', views.profile, name='user-profile'),
     path('profile/update/', views.profile_update,
          name='user-profile-update'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'),
+    name='user-logout'),
 ]
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
