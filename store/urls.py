b49be363110738211fbd5a0d6571a51d8f51cd21
rajon-git <rajon.zhsust15@gmail.com>
2024-09-09 18:36:12 +0600

    update product add

--- store/urls.py
+++ store/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from .views import (
     ProductListView,
     ProductCreateView,
-    ProductUpdateView
+    ProductUpdateView,
+    ProductDeleteView
 )
 
 urlpatterns = [
     path('products/',ProductListView.as_view(), name="productslist"),
     path('new-product/', ProductCreateView.as_view(), name='product-create'),
     path('product/<slug:slug>/update/', ProductUpdateView.as_view(), name='product-update'),
+    path('product/<slug:slug>/delete/', ProductDeleteView.as_view(), name='product-delete'),
 ]
--- store/views.py
+++ store/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
 )
 from .models import *
 from django_tables2.export.views import ExportMixin
@@ -36,3 +37,11 @@ class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+    
+class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Item
+    template_name = 'store/productdelete.html'
+    success_url = '/products'
+
+    def test_func(self):
+        item = self.get_object()
