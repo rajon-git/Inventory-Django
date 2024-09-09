c0a34ff5cc5480ab9756dc2e5d2ac8b0380134fe
rajon-git <rajon.zhsust15@gmail.com>
2024-09-09 17:31:12 +0600

    category model add

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django_extensions.db.fields import AutoSlugField
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = AutoSlugField(unique=True, populate_from = 'name')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
